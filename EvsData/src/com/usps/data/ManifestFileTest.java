782147db4ab14d41ca1a3aeda990107652f09884
mvattipulusu <mvattipulusu@crgt.com>
2014-01-28 11:54:34 -0800

    Checkin

--- EvsData/src/com/usps/data/ManifestFileTest.java
+++ EvsData/src/com/usps/data/ManifestFileTest.java
@@ -1,5 +1,8 @@
 package com.usps.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.evs.data.util.DataUtil;
 import com.usps.evs.vo.ManifestDetail;
 import com.usps.evs.vo.ManifestHeader;
@@ -13,13 +16,21 @@ public class ManifestFileTest {
 		// TODO Auto-generated method stub
 		String H1 = "H1|9275012821237546329520|1|20131225|121303||20260|0000|||||020|201312250000|9999|2.0.1|000000049|128212375|";
 		String D1 = "D1|4202026000009217912821237522693450|FC|179|C01|20260|0000||||||128212375||||||||||000|0000|1|01|20148|||0001000|P|||1|000001191|3|3D|N|00|00000|00000|00000|000000||000000||000000||000000||000000||000000|0000000|000000|0000||0000000||0000000|||0000000|000000000||0||N|N|1|00||||||||||||||||";
+	    List<ManifestDetail> details = new ArrayList<ManifestDetail>();
+	    
 		System.out.println(DataUtil.getFileVersion(H1));
-		ManifestHeaderProcessor header = new ManifestHeaderProcessor();
-		ManifestHeader header1 = header.parseHeaderLine(H1);
-		ManifestDetailProcessor detail = new ManifestDetailProcessor();
-		ManifestDetail detail1 = detail.parseDetailLine(D1);
-		System.out.println(header1.toString());System.out.println(detail1.toString());
+		ManifestHeaderProcessor hProcessor = new ManifestHeaderProcessor();
+		ManifestHeader header = hProcessor.parseHeaderLine(H1);
 		
+		ManifestDetailProcessor dProcessor = new ManifestDetailProcessor();
+		ManifestDetail detail = dProcessor.parseDetailLine(D1);
+		
+		details.add(detail);
+		header.setDetails(details);
+
+		//insert header to gae;
+		
+		System.out.println(header.toString());System.out.println(detail.toString());
 		
 	}
 
